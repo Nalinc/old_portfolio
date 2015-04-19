ddd40c9ef646c8d673e2d4653bcda59e144b290a
nalinc <nalin.chhibber@gmail.com>
2015-04-19 23:55:34 +0530

    render minifed skill-circle for small devices

--- js/init.js
+++ js/init.js
@@ -6,15 +6,32 @@ var o = {
 		return Math.floor((Math.random()*(u-l+1))+l);
 	},
 	diagram: function(){
-		var r = Raphael('diagram', 600, 600),
-			rad = 30,
+
+		if(screen.width < 640){
+			dimension = 400; 
+			radValue = 20;
+			radInc = 14;
+			strokeValue = 12
+			strokeValueHover = 15;
+			fontStyle = '10px Arial'
+		}
+		else{
+			dimension = 600; 
+			radValue = 30;
+			radInc = 20;
+			strokeValue = 18
+			strokeValueHover = 20;
+			fontStyle = '12px Arial'
+		}
+		var r = Raphael('diagram', dimension, dimension),
+			rad = radValue,
 			defaultText = 'Skills',
 			speed = 100;
 		
-		r.circle(300, 300, 40).attr({ stroke: 'none', fill: '#f6f6f6' });
+		r.circle((dimension/2), (dimension/2), radValue).attr({ stroke: 'none', fill: '#f6f6f6' });
 		
-		var title = r.text(300, 300, defaultText).attr({
-			font: '12px Arial',
+		var title = r.text((dimension/2), (dimension/2), defaultText).attr({
+			font: fontStyle,
 			fill: '#000'
 		}).toFront();
 		
@@ -24,10 +41,10 @@ var o = {
 				random = o.random(91, 240),
 				a = (random-alpha) * Math.PI/180,
 				b = random * Math.PI/180,
-				sx = 300 + rad * Math.cos(b),
-				sy = 300 - rad * Math.sin(b),
-				x = 300 + rad * Math.cos(a),
-				y = 300 - rad * Math.sin(a),
+				sx = (dimension/2) + rad * Math.cos(b),
+				sy = (dimension/2) - rad * Math.sin(b),
+				x = (dimension/2) + rad * Math.cos(a),
+				y = (dimension/2) - rad * Math.sin(a),
 				path = [['M', sx, sy], ['A', rad, rad, 0, +(alpha > 180), 1, x, y]];
 			return { path: path, stroke: color }
 		}
@@ -38,18 +55,18 @@ var o = {
 				value = t.find('.percent').val(),
 				text = t.find('.text').text();
 			
-			rad += 20;	
-			var z = r.path().attr({ arc: [value, color, rad], 'stroke-width': 18 });
+			rad += radInc;	
+			var z = r.path().attr({ arc: [value, color, rad], 'stroke-width': strokeValue });
 			
 			z.mouseover(function(){
-                this.animate({ 'stroke-width': 20, opacity: .80 }, 800, 'elastic');
+                this.animate({ 'stroke-width': strokeValueHover, opacity: .80 }, 800, 'elastic');
                 if(Raphael.type != 'VML') //solves IE problem
 				this.toFront();
 				title.stop().animate({ opacity: 0 }, speed, '>', function(){
 					this.attr({ text: text + '\n' + value + '%' }).animate({ opacity: 1 }, speed, '<');
 				});
             }).mouseout(function(){
-				this.stop().animate({ 'stroke-width': 18, opacity: 1 }, speed*4, 'elastic');
+				this.stop().animate({ 'stroke-width': strokeValue, opacity: 1 }, speed*4, 'elastic');
 				title.stop().animate({ opacity: 0 }, speed, '>', function(){
 					title.attr({ text: defaultText }).animate({ opacity: 1 }, speed, '<');
 				});	
