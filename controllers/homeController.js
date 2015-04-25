0ead37e3a44d01399512fe1cb96cc06e1a582528
nalinc <nalin.chhibber@gmail.com>
2015-04-25 18:43:56 +0530

    Move timline to profile

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,22 +6,7 @@ define(['custom','plugins','texteffect'],function(){
 
 		$scope.$on('$viewContentLoaded', function(){
 
-				$rootScope.showLoader= false;
-
-				var timeline = $("#timeline-carousel"),
-					timelineOpts = {
-					slideSpeed: 200,
-					paginationSpeed: 200,
-					rewindSpeed: 800,
-					// singleItem: true,
-					items : 3,
-					itemsDesktop: [1170, 3],
-					itemsMobile: [480, 1],
-					autoPlay: true,
-					pagination: false, // disable default pagination
-					// responsive: true
-				};
-			timeline.owlCarousel(timelineOpts);
+			$rootScope.showLoader= false;
 
 		});
 	    $(".subtitle").scrambledWriter();
--- controllers/skillController.js
+++ controllers/skillController.js
@@ -5,6 +5,23 @@ define([''],function(){
 	 	$scope.$on('$viewContentLoaded', function(){
 		// $("#circular-skills").skillChart()
 
+
+				var timeline = $("#timeline-carousel"),
+					timelineOpts = {
+					slideSpeed: 200,
+					paginationSpeed: 200,
+					rewindSpeed: 800,
+					// singleItem: true,
+					items : 3,
+					itemsDesktop: [1170, 3],
+					itemsMobile: [480, 1],
+					autoPlay: true,
+					pagination: false, // disable default pagination
+					// responsive: true
+				};
+			timeline.owlCarousel(timelineOpts);
+
+
   			require(["init"], function() {
         // do some stuff with these require()'d dependencies
         		if(!$("svg").height())
