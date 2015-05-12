a62233953758af2ea4d0128aab99d0f4aac60b03
NC <nalin.chhibber@gmail.com>
2015-05-13 00:14:09 +0530

    add middleware for node_modules

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/client'))
 
-//app.use(express.staticProvider(__dirname + '/public'));
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
 app.get('/', function(request, response) {
   response.render('index.html')
