136bd7cf00d0b67c9cf35b733a7060c1bc9d4be8
nalinc <nalin.chhibber@gmail.com>
2015-05-15 15:56:37 +0530

    fix static file reference (reverted from commit 4598c08083442f5ea105cbf5636c1136fa414fd4)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/client'))
 
-app.use('../node_modules',  express.static(__dirname + '/node_modules'));
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
 app.get('/', function(request, response) {
   response.render('index.html')
