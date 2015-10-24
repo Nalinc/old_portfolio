ac9d33c55efaf9fd694c9b07713339bfec521ddc
NC <nalin.chhibber@gmail.com>
2015-10-24 17:45:36 +0530

    major structural changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,43 +1,102 @@
-define(['routes','services/dependencyResolverFor','angular-animate','ngHeader','ngFooter'], function(config, dependencyResolverFor)
+define(['angular-animate','wow'], function()
 {
-    var app = angular.module('app', ['ngRoute','appHeader','appFooter','ngAnimate']);
+    var app = angular.module('app', ['ngRoute','ngAnimate']);
 
-    app.config(
-    [
-        '$routeProvider',
-        '$locationProvider',
-        '$controllerProvider',
-        '$compileProvider',
-        '$filterProvider',
-        '$provide',
+    app.config(function($routeProvider){
 
-        function($routeProvider, $locationProvider, $controllerProvider, $compileProvider, $filterProvider, $provide)
-        {
-            app.controller = $controllerProvider.register;
-            app.directive  = $compileProvider.directive;
-            app.filter     = $filterProvider.register;
-            app.factory    = $provide.factory;
-            app.service    = $provide.service;
-
+            $routeProvider
+                .when('/', {
+                templateUrl: '/views/home.html',
+                controller: 'homeController',
+              })
+                .when('/about', {
+                templateUrl: '/views/about.html',
+                controller: 'aboutController'
+              })
+                .when('/profile', {
+                templateUrl: '/views/profile.html',
+                controller: 'profileController'
+              })
+                .when('/lab', {
+                templateUrl: '/views/lab.html',
+                controller: 'labController'
+              })
+                .when('/connect', {
+                templateUrl: '/views/connect.html',
+                controller: 'connectController'
+              })
+                .otherwise({
+                redirectTo: '/'
+              });
             /*  $locationProvider.html5Mode({
                 enabled: true,
                 requireBase: false
               });*/
+        });
 
-            if(config.routes !== undefined)
-            {
-                angular.forEach(config.routes, function(route, path)
-                {
-                    $routeProvider.when(path, {templateUrl:route.templateUrl, resolve:dependencyResolverFor(route.dependencies)});
-                });
-            }
-
-            if(config.defaultRoutePaths !== undefined)
-            {
-                $routeProvider.otherwise({redirectTo:config.defaultRoutePaths});
-            }
+    /*-----Controllers----- */
+    app.controller('homeController', function($scope,$rootScope){
+            $rootScope.showLoader= false;
+            wow = new WOW({animateClass: 'animated', mobile: true});
+            wow.init();
+    });
+    app.controller("aboutController",function($scope,$rootScope){
+            $(".subtitle").scrambledWriter();
+    });
+    app.controller("profileController",function($scope){
+        
+    });
+    app.controller('labController', function($scope){
+        $scope.open=function(){
+            alert()
         }
-    ]);
+        $('ul#filter a').click(function() {
+                $(this).css('outline','none');
+                $('ul#filter .current').removeClass('current');
+                $(this).parent().addClass('current');
+                
+                var filterVal = $(this).text().toLowerCase().replace(' ','-');
+                    
+                if(filterVal == 'all') {
+                  $('ul#portfolio li.hidden').fadeIn('slow').removeClass('hidden');
+                } else {
+                  $('ul#portfolio li').each(function() {
+                    if(!$(this).hasClass(filterVal)) {
+                      $(this).fadeOut('normal').addClass('hidden');
+                    } else {
+                      $(this).fadeIn('slow').removeClass('hidden');
+                    }
+                  });
+                }
+            return false;
+        });
+    });
+    app.controller("connectController",function($scope){
+        
+    });    
+
+    /*-----Directives----- */
+    app.directive("ngFooter",function(){
+        return{
+            restrict:'E',
+            templateUrl:'views/ng-footer.html'
+        };
+    });    
+    app.directive("ngHeader",function(){
+        return{
+            restrict:'E',
+            templateUrl:'views/ng-header.html',
+            controller:function($rootScope,$timeout){
+                $rootScope.$on('$routeChangeStart', function(event, next, current) {
+                    $rootScope.showLoader= true;
+                });
+                $rootScope.$on('$routeChangeSuccess', function(event, next, current) {
+                    $rootScope.showLoader= false;
+//                    $timeout(function(){$rootScope.showLoader= false;}, 1000);
+                });         
+            }
+        };
+    });
 
    return app;
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
+require.config({
+  paths:{
+    'angular':[
+                'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min',
+                '../node_modules/angular/angular.min'
+              ],
+    'jquery':'/node_modules/jquery/dist/jquery.min',
+    'angular-route':[
+                      'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-route',
+                      '../node_modules/angular-route/angular-route.min'
+                    ],
+    'app':'app',
+    'wow':'lib/wow',
+    'texteffect':'lib/texteffect',
+    'angular-animate':'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-animate'
+  },
+
+  shim: {
+    'app': { deps: ['angular', 'angular-route','texteffect'] },
+    'angular-route': { deps: ['angular'] },
+    'angular-animate':{ deps: ['angular']},
+    'texteffect':{ deps: ['jquery']}
+  }
+});
+
 require(['app'], function(app){
         angular.bootstrap(document, ['app']);
     }
